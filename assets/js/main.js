14e2ca7e1cc914a4561201ba1841a8e70d9a8d4c
Omar <omaar_5@hotmail.com>
2021-06-05 14:31:56 +0200

    feat: Header part-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,27 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+navToggle && navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+});
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
+navClose && navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+});
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+};
+navLink.forEach(e => e.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
@@ -30,10 +41,10 @@
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
