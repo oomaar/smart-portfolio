712a56e46f2f5ffc1619f7629d8200c61a7cf0ac
Omar <omaar_5@hotmail.com>
2021-06-06 00:56:52 +0200

    feat: complete Skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,22 @@ function linkAction() {
 navLink.forEach(e => e.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    };
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    };
+};
+
+skillsHeader.forEach(e => e.addEventListener('click', toggleSkills));
 
 /*==================== QUALIFICATION TABS ====================*/
 
