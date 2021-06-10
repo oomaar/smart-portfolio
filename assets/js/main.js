96b93d3b31c0bd44f0d690b8b46d35961f31c876
Omar <omaar_5@hotmail.com>
2021-06-10 16:20:06 +0200

    feat: scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,7 +120,27 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
 
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(section => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+        sectionId = section.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.remove('active-link');
+        };
+    });
+};
+
+window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 
